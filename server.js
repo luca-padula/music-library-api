d946a9c263b87c52f6d13612fcf443c253833770
Luca Padula <lucamp9843@gmail.com>
2021-05-08 12:24:16 -0400

    add boilerplate server file.

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('hi')
+const express = require("express")
+const cors = require("cors")
+require("dotenv").config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const port = process.env.HTTP_PORT
+
+app.listen(port, () => console.log(`app listening on port ${port}`))
