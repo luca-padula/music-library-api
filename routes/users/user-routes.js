aff3572c216ae50a348b659f42e2d4e995894c50
Luca Padula <lucamp9843@gmail.com>
2021-05-08 16:27:11 -0400

    add routes folder

--- routes/users/user-routes.js
+++ routes/users/user-routes.js
@@ -1,12 +1,8 @@
 const express = require("express")
-const userRoutes = require("./users/user-routes")
-
 const router = express.Router()
 
 router.get("/", (req, res) => {
-   res.json({ version: "1.0.0" })
+   res.json({ message: "getting all users" })
 })
-
-router.use("/users", userRoutes)
 
 module.exports = router
